04380d077b921c028c81a79f83a621ad76c001ab
NguyenTC <nguyentc@splus-software.com.vn>
2023-07-27 22:08:36 +0700

    auth_user

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,19 +11,19 @@
 
 <div class="container">
     <div id="content">
-
-        <form action="#" method="post" class="beta-form-checkout">
+        <form method="POST" action="{{ route('login') }}" class="beta-form-checkout">
+            @csrf
             <div class="row">
                 <div class="col-sm-3"></div>
                 <div class="col-sm-6">
 
                     <div class="form-block">
                         <label for="email"> Tài Khoản </label>
-                        <input type="email" id="email" required>
+                        <input type="text" id="email" name="email" required>
                     </div>
                     <div class="form-block">
                         <label for="phone"> Mật Khẩu</label>
-                        <input type="text" id="phone" required>
+                        <input type="text" id="phone" name="password" required>
                     </div>
                     <div class="form-block">
                         <button type="submit" class="btn btn-primary">Đăng nhập</button>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/',[HomeController::class,'index'])->name('homePage');
-Route::get('/login',[LoginController::class,'loginForm'])->name('login');
-Route::get('/register',[LoginController::class,'register'])->name('register');
+Route::match(['get', 'post'], '/login', [LoginController::class, 'loginForm'])->name('login');
+Route::get('/register',[RegisterController::class,'registerForm'])->name('register');
+Route::post('/register',[RegisterController::class,'store'])->name('store.register');
